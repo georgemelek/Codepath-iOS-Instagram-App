03c11874ac1a6bb8878caf9ad9a4001ef591798c
georgemelek <melekgeorge@outlook.com>
2021-03-20 13:35:21 -0400

    added feature-displayed comments

---
+++ instagram_clone/CommentCell.swift
@@ -0,0 +1,29 @@
+//
+//  CommentCell.swift
+//  instagram_clone
+//
+//  Created by George Melek on 3/20/21.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
